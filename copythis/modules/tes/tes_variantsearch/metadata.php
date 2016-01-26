c3e30dff62eafeff5d21fb348ca7be9e382d3e47
Fabian Kaufmann <f.kaufmann@te-solutions.de>
2016-01-26 10:36:56 +0100

    1.2.0
    grouping into vendor folder ‚tes‘

--- copythis/modules/tes/tes_variantsearch/metadata.php
+++ copythis/modules/tes/tes_variantsearch/metadata.php
@@ -1,13 +1,13 @@
 <?php
 
-$sMetadataVersion = '1.0';
+$sMetadataVersion = '1.2.0';
  
 $aModule = array(
     'id'           => 'tes_variantsearch',
-    'title'        => '<img src="../modules/tes_variantsearch/out/img/tes_icon.png" width="15" height="15"> TESolutions - Variantensuche',
+    'title'        => '<img src="../modules/tes/tes_variantsearch/out/img/tes_icon.png" width="15" height="15"> TESolutions - Variantensuche',
     'description'  => 'Ermöglicht die Suche von Variantenartikeln. Bitte beachten Sie, dass diese Funktion bei einer hohen Anzahl von Artikeln und Varianten zu Performance-Problemen führen kann. Dies ist ein kostenloses Modul der Firma TESolutions.',
     'thumbnail'    => 'out/img/tesolutions.jpg',
-    'version'      => '1.0',
+    'version'      => '1.2.0',
     'author'       => 'Fabian Kaufmann',
     'url'          => 'http://www.te-solutions.de',
     'email'        => 'f.kaufmann@te-solutions.de',
